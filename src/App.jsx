8c343950ae5450d0004c0a4a8b6bc53771330d39
Hamse Abdi <hamse.sulup@gmail.com>
2025-07-11 17:29:04 +0300

    completed hero part

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
       <div className="pattern"/>
       <div className="wrapper">
         <header>
+          <img src='./hero.png' alt='hero banner'/>
           <h1> Find <span className="text-gradient">Movies</span> You'll Enjoy Whitout the Hassle</h1>
         </header>
       </div>
